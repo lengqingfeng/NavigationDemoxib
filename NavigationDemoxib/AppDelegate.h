74654335c0e4a0f36d3279b25990edcaf07960c3
lengqingfeng <lengshengren@lengshengrentekiiMac.local>
2013-11-19 11:12:39 +0800

    Initial Commit

---
+++ NavigationDemoxib/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NavigationDemoxib
+//
+//  Created by lengshengren on 13-11-19.
+//  Copyright (c) 2013年 lengshengren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
